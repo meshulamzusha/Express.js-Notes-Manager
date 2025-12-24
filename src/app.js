3ead693de25bb8741c7d720bd9d1cbe5aa74adbb
Aviel Oiknine <avieloikpro@gmail.com>
2025-12-24 17:48:19 +0200

    userRegister fonction

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import healthRouter from './routes/healthRouter.js';
+import registerRouter from './routes/userRegister.js';
 
 const app = express();
 const port = 3000;
@@ -8,8 +9,11 @@ app.use(express.json())
 
 
 app.use('/health', healthRouter);
+app.use('/register', registerRouter);
 
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
 })
+
+console.log('App started');
